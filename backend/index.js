9dea1f30fb56fa273a5ff89da5fbe39a24457423
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2022-09-22 16:28:06 +0800

    [SELS-TASK] Create Login API route [BE]

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
+const db = require('./models')
+const authRoutes = require('./routes/auth')
+const usersRoutes = require('./routes/users')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 8080;
 
-app.use((req, res, next) => {
-    res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`);
-})
+app.use(authRoutes);
+app.use('/users', usersRoutes);
+
+db.sequelize.authenticate()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log('Cannot connect to the database!')
+    })
+
---
+++ backend/routes/users.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const usersController = require('../controllers/users')
+
+const router = Router();
+
+router.get('/', usersController.getUsers)
+
+module.exports = router;
