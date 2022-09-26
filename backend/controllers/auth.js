ad697fe2c33f77a899a0c72c91d385a1a3bcb3aa
jules-lucero <111732984+jules-lucero@users.noreply.github.com>
2022-09-26 11:41:47 +0800

    [SELS-TASK] Create Sign Up API route [BE] (#3)
    * [SELS-TASK] Create Sign Up API route [BE]
    
    * Filter user object for the response
    
    * Create README.md
    
    * Change Postman link

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,6 +4,8 @@ const ResponseHelper = require("../helpers/response");
 
 const { User } = require("../models");
 
+const salt = 11;
+
 module.exports = {
   login: async (req, res) => {
     const { email, password } = req.body;
@@ -23,7 +25,36 @@ module.exports = {
       );
       return;
     }
-
     res.send(ResponseHelper.generateResponse(200, "Success", { user }));
+  },
+  signup: async (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      res.send(
+        ResponseHelper.generateResponse(401, "Email is already in use.")
+      );
+      return;
+    }
+
+    const hash = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      first_name,
+      last_name,
+      email,
+      password: hash,
+      user_type: "user",
+      avatar_url: "default_img.jpeg",
+    });
+
+    res.send(
+      ResponseHelper.generateResponse(200, "Success", {
+        id: user.id,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      })
+    );
   },
 };
