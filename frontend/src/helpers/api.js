93ece24a4b3406e45c7412919f262bf572a6dd0c
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2022-10-04 12:17:40 +0800

    [SELS-TASK] Display a follow or unfollow button [FE]

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -7,4 +7,43 @@ const api = axios.create({
   timeout: 1000,
 });
 
+export const getUserFeed = async (user_id, setUserFeed) => {
+  const { data } = await api.get("/users/activity-logs", {
+    params: {
+      user_id,
+    },
+  });
+  setUserFeed(data.data.activity_logs);
+};
+
+export const getLearnings = async (user_id, setLearnings) => {
+  const { data } = await api.get("/users/learn-count/", {
+    params: {
+      user_id,
+    },
+  });
+  setLearnings(data.data);
+};
+
+export const getUserProfile = async (user_id, setUserProfile) => {
+  const { data } = await api.get("/users/profile", {
+    params: {
+      user_id,
+    },
+  });
+  const {
+    data: { followers, following, activity_logs },
+  } = data;
+
+  setUserProfile(followers, following, activity_logs);
+};
+
+export const toggleFollow = async (follower_id, following_id) => {
+  const { data } = await api.post("/users/toggle-follow", {
+    follower_id,
+    following_id,
+  });
+  return data;
+};
+
 export default api;
