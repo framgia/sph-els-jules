9c5230f65a977d3bd135c4936dcc282896ca391d
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2022-11-09 14:32:20 +0800

    Refactor testing structure

--- backend/tests/controllers/user/lessons.test.js
+++ backend/tests/controllers/user/lessons.test.js
@@ -1,7 +1,6 @@
 const httpMocks = require("node-mocks-http");
 
 const lessonsController = require("../../../controllers/user/lessons");
-
 let req;
 let res;
 
@@ -10,14 +9,36 @@ beforeEach(() => {
   res = httpMocks.createResponse();
 });
 
-describe("GET /lessons", () => {
-  it("should fetch all the lessons and corresponding scores of the user", async () => {
-    req.currentUserId = 1;
-    await lessonsController.getLessons(req, res);
-    const data = res._getData();
+describe("When calling getLessons function", () => {
+  describe("When fetching lessons is successful", () => {
+    beforeEach(async () => {
+      req.currentUserId = 1;
+      await lessonsController.getLessons(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lessons");
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(res._getData().meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(Array.isArray(res._getData().data.lessons)).toBeTruthy();
+      res._getData().data.lessons.forEach((lesson) => {
+        expect(lesson).toMatchObject({
+          id: expect.any(Number),
+          title: expect.any(String),
+          description: expect.any(String),
+          deleted_at: expect.toSatisfy(
+            (received) => received === null || expect(received).any(Date)
+          ),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+          Lesson_words: expect.any(Array),
+        });
+      });
+    });
   });
 });
--- backend/tests/controllers/user/words.test.js
+++ backend/tests/controllers/user/words.test.js
@@ -10,14 +10,39 @@ beforeEach(() => {
   res = httpMocks.createResponse();
 });
 
-describe("GET /words/user", () => {
-  it("should fetch all words learned of a user", async () => {
-    req.query.user_id = 1;
-    await wordsController.getWordsLearnedByUserId(req, res);
-    const data = res._getData();
+describe("When calling getWordsLearnedByUserId function", () => {
+  describe("When fetching words is successful", () => {
+    beforeEach(async () => {
+      req.query.user_id = 1;
+      await wordsController.getWordsLearnedByUserId(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("words_learned");
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(res._getData().meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(Array.isArray(res._getData().data.words_learned)).toBeTruthy();
+      res._getData().data.words_learned.forEach((word) => {
+        expect(word).toMatchObject({
+          id: expect.any(Number),
+          question: expect.any(String),
+          correct_answer: expect.any(String),
+          choice1: expect.any(String),
+          choice2: expect.any(String),
+          choice3: expect.any(String),
+          choice4: expect.any(String),
+          deleted_at: expect.toSatisfy(
+            (received) => received === null || expect(received).any(Date)
+          ),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        });
+      });
+    });
   });
 });
