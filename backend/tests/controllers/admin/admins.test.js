a2c39ffa3156d80a35874e53bf50be435626495d
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2022-11-10 16:24:29 +0800

    Refactor tests structure for admin controllers

--- backend/tests/controllers/admin/admins.test.js
+++ backend/tests/controllers/admin/admins.test.js
@@ -10,12 +10,33 @@ beforeEach(() => {
   res = httpMocks.createResponse();
 });
 
-describe("GET /admins", () => {
-  it("should fetch all admins", async () => {
-    await adminsController.getAdmins(req, res);
-    const data = res._getData();
+describe("When calling getAdmins function", () => {
+  describe("When fetching admins is successfully", () => {
+    beforeEach(async () => {
+      await adminsController.getAdmins(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(res._getData().meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(Array.isArray(res._getData().data.admins)).toBeTruthy();
+      res._getData().data.admins.forEach((admin) => {
+        expect(admin).toMatchObject({
+          id: expect.any(Number),
+          first_name: expect.any(String),
+          last_name: expect.any(String),
+          email: expect.any(String),
+          user_type: expect.any(String),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        });
+      });
+    });
   });
 });
--- backend/tests/controllers/admin/lessons.test.js
+++ backend/tests/controllers/admin/lessons.test.js
@@ -12,99 +12,218 @@ beforeEach(() => {
   res = httpMocks.createResponse();
 });
 
-describe("GET /admin/lessons", () => {
-  it("should fetch all the lessons", async () => {
-    await lessonsController.getLessons(req, res);
-    const data = res._getData();
+describe("When calling getLessons function", () => {
+  describe("When fetching lessons is successful", () => {
+    beforeEach(async () => {
+      await lessonsController.getLessons(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lessons");
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(res._getData().meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(Array.isArray(res._getData().data.lessons)).toBeTruthy();
+      res._getData().data.lessons.forEach((lesson) => {
+        expect(lesson.dataValues).toMatchObject({
+          id: expect.any(Number),
+          title: expect.any(String),
+          description: expect.any(String),
+          deleted_at: expect.toSatisfy(
+            (received) => received === null || expect(received).any(Date)
+          ),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+          Lesson_words: expect.any(Array),
+        });
+      });
+    });
   });
 });
 
-describe("GET /admin/lessons/id", () => {
-  it("should fetch a lesson by id", async () => {
-    req.query.id = 1;
-    await lessonsController.getLessonById(req, res);
-    const data = res._getData();
+describe("When calling getLessonById function", () => {
+  describe("When fetching a lesson is successful", () => {
+    beforeEach(async () => {
+      req.query.id = 1;
+      await lessonsController.getLessonById(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lesson");
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(res._getData().meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(res._getData().data.lesson).toMatchObject({
+        id: expect.any(Number),
+        title: expect.any(String),
+        description: expect.any(String),
+        deleted_at: expect.toSatisfy(
+          (received) => received === null || expect(received).any(Date)
+        ),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        Lesson_words: expect.any(Array),
+      });
+    });
   });
 
-  it("should error when lesson is not found", async () => {
-    req.query.id = 17;
-    await lessonsController.getLessonById(req, res);
-    const data = res._getData();
+  describe("When fetching a lesson that does not exist", () => {
+    beforeEach(async () => {
+      req.query.id = 17;
+      await lessonsController.getLessonById(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(404);
-  });
-});
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
 
-describe("POST /admin/lessons", () => {
-  it("should create a new lesson", async () => {
-    req.body = { title: "Basic 100", description: "Lorem Ipsum" };
-    await lessonsController.createLesson(req, res);
-    const data = res._getData();
-
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lesson");
-
-    const lesson = await Lesson.findByPk(data.data.lesson.id);
-    expect(lesson).not.toBeNull();
-  });
-});
-
-describe("PUT /admin/lessons/update/id", () => {
-  it("should update an existing lesson", async () => {
-    req.query.id = 1;
-    req.body = { title: "Basic 600", description: "Lorem Ipsum" };
-    await lessonsController.updateLessonById(req, res);
-    const data = res._getData();
-
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lesson");
-
-    const lesson = await Lesson.findByPk(data.data.lesson.id);
-    expect(lesson.title).toEqual("Basic 600");
-  });
-
-  it("should error when lesson is not found", async () => {
-    req.query.id = 17;
-    req.body = { title: "On Vacation", description: "Lorem Ipsum" };
-    await lessonsController.updateLessonById(req, res);
-    const data = res._getData();
-
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(404);
+    it("should return meta code of 404", () => {
+      expect(res._getData().meta.code).toEqual(404);
+    });
   });
 });
 
-describe("DELETE /admin/lessons/delete/id", () => {
-  it("should update an existing lesson", async () => {
-    req.query.id = 1;
-    await lessonsController.deleteLessonById(req, res);
-    const data = res._getData();
+describe("When calling createLesson function", () => {
+  describe("When creating a lesson is successful", () => {
+    let data;
+    beforeAll(async () => {
+      req.body = { title: "Basic 100", description: "Lorem Ipsum" };
+      await lessonsController.createLesson(req, res);
+      data = res._getData();
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(200);
-    expect(data.data).toHaveProperty("lesson");
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
 
-    const lesson = await Lesson.findByPk(data.data.lesson.id);
-    expect(lesson?.deleted_at).not.toBeNull();
+    it("should return meta code of 200", () => {
+      expect(data.meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(data.data.lesson).toMatchObject({
+        id: expect.any(Number),
+        title: expect.any(String),
+        description: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      });
+    });
+
+    it("should be inserted to the database", async () => {
+      const lesson = await Lesson.findByPk(data.data.lesson.id);
+      expect(lesson).not.toBeNull();
+    });
+  });
+});
+
+describe("When calling updateLessonById function", () => {
+  describe("When updating a lesson is successful", () => {
+    let data;
+    beforeEach(async () => {
+      req.query.id = 1;
+      req.body = { title: "Basic 600", description: "Lorem Ipsum" };
+      await lessonsController.updateLessonById(req, res);
+      data = res._getData();
+    });
+
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(data.meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(data.data.lesson).toMatchObject({
+        id: expect.any(Number),
+        title: expect.any(String),
+        description: expect.any(String),
+        deleted_at: expect.toSatisfy(
+          (received) => received === null || expect(received).any(Date)
+        ),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      });
+    });
+
+    it("should be updated in the database", async () => {
+      const lesson = await Lesson.findByPk(data.data.lesson.id);
+      expect(lesson.title).toEqual("Basic 600");
+    });
   });
 
-  it("should error when lesson is not found", async () => {
-    req.query.id = 17;
-    await lessonsController.deleteLessonById(req, res);
-    const data = res._getData();
+  describe("When the lesson is not found", () => {
+    beforeEach(async () => {
+      req.query.id = 17;
+      await lessonsController.updateLessonById(req, res);
+    });
 
-    expect(res._getStatusCode()).toEqual(200);
-    expect(data.meta.code).toEqual(404);
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 404", () => {
+      expect(res._getData().meta.code).toEqual(404);
+    });
+  });
+});
+
+describe("When calling deleteLessonById function", () => {
+  describe("When deleting a lesson is successful", () => {
+    let data;
+    beforeAll(async () => {
+      req.query.id = 1;
+      await lessonsController.deleteLessonById(req, res);
+      data = res._getData();
+    });
+
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 200", () => {
+      expect(data.meta.code).toEqual(200);
+    });
+
+    it("should have a correct body structure properties", () => {
+      expect(data.data.lesson).toMatchObject({
+        id: expect.any(Number),
+        title: expect.any(String),
+        description: expect.any(String),
+        deleted_at: expect.any(Date),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      });
+    });
+
+    it("should be soft deleted in the database", async () => {
+      const lesson = await Lesson.findByPk(data.data.lesson.id);
+      expect(lesson?.deleted_at).not.toBeNull();
+    });
+  });
+
+  describe("When the lesson is not found", () => {
+    beforeEach(async () => {
+      req.query.id = 17;
+      await lessonsController.deleteLessonById(req, res);
+    });
+
+    it("should return status of 200", () => {
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it("should return meta code of 404", () => {
+      expect(res._getData().meta.code).toEqual(404);
+    });
   });
 });
